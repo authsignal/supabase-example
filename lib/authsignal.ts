bfbd10f8885b1750e96edb197f91613bf53de1b8
Chris Fisher <chris.i.fisher@gmail.com>
2022-08-23 12:37:33 +1200

    Update callback url port

--- lib/authsignal.ts
+++ lib/authsignal.ts
@@ -8,6 +8,6 @@ if (!secret) {
 
 const redirectUrl = process.env.SITE_URL
   ? `${process.env.SITE_URL}/api/callback`
-  : "http://localhost:3001/api/callback";
+  : "http://localhost:3000/api/callback";
 
 export const authsignal = new Authsignal({ secret, redirectUrl });
